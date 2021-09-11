bc2ead6f7a449e06c697bb39fd02c77d1dfb4b27
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-11 14:49:50 +0000

    Add modal for defensive programming

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialisation
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  // sidenav initialisation
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.modal');
+  M.Modal.init();
+});
