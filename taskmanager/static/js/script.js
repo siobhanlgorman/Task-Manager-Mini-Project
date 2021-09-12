78d503eb6cd93139202dc766ac65914828e04e34
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-12 10:00:00 +0000

    Add form to submit new tasks to database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,9 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
   // sidenav initialisation
   let sidenav = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sidenav);
+
+  // let modal= document.querySelectorAll('.modal');
+  // M.Modal.init(modal);
+
+  // datepicker initialisation
+  let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+  // select initialisation
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.modal');
-  M.Modal.init();
-});
